baa80bd2ff31f76188830b282e24d81a6990c4ed
fernastereo <fernandoecueto@gmail.com>
2020-07-03 19:00:44 -0500

    Embedding partials

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,8 +8,7 @@ import (
 	"strconv"
 )
 
-
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -18,23 +17,24 @@ func home(w http.ResponseWriter, r *http.Request){
 	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
 	}
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		http.Error(w, "Internal Server Error 1", 500)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
 		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		http.Error(w, "Internal Server Error 2", 500)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request){
+func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -44,7 +44,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request){
+func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method not Allowed", 405)
@@ -53,5 +53,3 @@ func createSnippet(w http.ResponseWriter, r *http.Request){
 
 	w.Write([]byte("Create a new snippet"))
 }
-
-
