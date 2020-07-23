6da04923115264e117abeed37d12294c52716bed
fernastereo <fernandoecueto@gmail.com>
2020-07-23 09:02:00 -0500

    Dependency injection for error logger

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,46 +3,45 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+// Change the signature for the home handler so it is defined as a method against
+// *application
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
-	// Initialize a slice containing the paths to the three files. Note that the
-	// home.page.tmpl file must be the *first* file in the slice
 	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
 	}
 
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error response to th user.
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		// Because the home handler function is now a method against application
+		// it can access its fields, includig the error logger. We'll write the log
+		// message to this instead of the standard logger.
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error 1", 500)
 		return
 	}
 
-	// We then use the Execute() method on the template set to write the template
-	// content as the response body. The last parameter to Execute() represents any
-	// dynamic data that we want to pass in, which for now we'll leave as nil
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		// Also update the code here to use the error logger from the application struct
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error 2", 500)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the showSnippet handler so it is defined as a method
+// aginst *application
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -52,7 +51,9 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the createSnippet handler so it is defined as a method
+// aginst *application
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method not Allowed", 405)
